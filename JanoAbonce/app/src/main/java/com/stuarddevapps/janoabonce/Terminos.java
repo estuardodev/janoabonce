f4ae84df874fb97881fb3e90ff637383c5b39bd3
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-06-13 12:42:22 -0600

    Se agregaron los terminos y politicas

--- JanoAbonce/app/src/main/java/com/stuarddevapps/janoabonce/Terminos.java
+++ JanoAbonce/app/src/main/java/com/stuarddevapps/janoabonce/Terminos.java
@@ -1,14 +1,67 @@
 package com.stuarddevapps.janoabonce;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class Terminos extends AppCompatActivity {
+
+    private WebView miweb;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terminos);
+
+        //ANUNCIOS
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+        miweb = findViewById(R.id.web1);
+        miweb.getSettings().setJavaScriptEnabled(true);
+        miweb.setWebViewClient(new WebViewClient());
+        miweb.loadUrl(getString(R.string.linkTerminos));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_back, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itb1:
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
