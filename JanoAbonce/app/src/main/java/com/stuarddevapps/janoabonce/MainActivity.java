72d565b0b1142884f7fc2a46d2d7e7a1cb9b25ba
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-06-12 22:20:07 -0600

    Se agrego el codigo del bottom

--- JanoAbonce/app/src/main/java/com/stuarddevapps/janoabonce/MainActivity.java
+++ JanoAbonce/app/src/main/java/com/stuarddevapps/janoabonce/MainActivity.java
@@ -1,14 +1,78 @@
 package com.stuarddevapps.janoabonce;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Menu Variables
+    private BottomNavigationView bnvMenu;
+    private Fragment fragment;
+    private FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+    //MENU BOTTOM
+    private void initView(){
+        bnvMenu = findViewById(R.id.bnvMenu);
+    }
+    private void initValues(){
+        manager = getSupportFragmentManager();
+        loadFirstFragment();
+    }
+    private void initListener(){
+        bnvMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int idMenu = item.getItemId();
+                switch (idMenu){
+                    case R.id.menu_eventos:
+                        getSupportActionBar().setTitle(getString(R.string.eventos));
+                        fragment = Eventos.newInstance();
+                        openFragment(fragment);
+                        return true;
+                    case R.id.menu_directo:
+                        getSupportActionBar().setTitle(getString(R.string.directos));
+                        fragment = Directo.newInstance();
+                        openFragment(fragment);
+                        return true;
+                    case R.id.menu_config:
+                        getSupportActionBar().setTitle(getString(R.string.config));
+                        fragment = Directo.newInstance();
+                        openFragment(fragment);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+    private void openFragment(Fragment fragment){
+        manager.beginTransaction().
+                replace(R.id.frameContainer, fragment)
+                .commit();
+    }
+    private void loadFirstFragment(){
+        getSupportActionBar().setTitle(R.string.app_name);
+        fragment = Eventos.newInstance();
+        openFragment(fragment);
     }
 }
