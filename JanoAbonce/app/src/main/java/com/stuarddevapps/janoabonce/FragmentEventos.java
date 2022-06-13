bcb254b08f78d4ba44cc1a8392804956404a71c1
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-06-13 01:08:56 -0600

    Se agregaron los eventos

--- JanoAbonce/app/src/main/java/com/stuarddevapps/janoabonce/FragmentEventos.java
+++ JanoAbonce/app/src/main/java/com/stuarddevapps/janoabonce/FragmentEventos.java
@@ -2,14 +2,30 @@ package com.stuarddevapps.janoabonce;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class FragmentEventos extends Fragment {
 
+    //Variables de Database
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    MyAdapter myAdapter;
+    ArrayList<User> list;
 
     public FragmentEventos() {
         // Required empty public constructor
@@ -30,7 +46,39 @@ public class FragmentEventos extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vista = inflater.inflate(R.layout.fragment_eventos, container, false);
+        recyclerView = vista.findViewById(R.id.userList);
+
+
+        //LLAMADAS
+        funcionDB();
 
         return vista;
+    }
+
+    private void funcionDB(){
+
+        database = FirebaseDatabase.getInstance().getReference("Users");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        list = new ArrayList<>();
+        myAdapter = new MyAdapter(getContext(),list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    User user  = dataSnapshot.getValue(User.class);
+                    list.add(user);
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
---
+++ JanoAbonce/app/src/main/java/com/stuarddevapps/janoabonce/User.java
@@ -0,0 +1,21 @@
+package com.stuarddevapps.janoabonce;
+
+public class User {
+    String firstName, lastName, age, hour;
+
+    public String getFirstname() {
+        return firstName;
+    }
+
+    public String getLastname() {
+        return lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+}
